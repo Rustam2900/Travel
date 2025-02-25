bd39519241887e9d8020168adec116f2df6af488
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2025-02-25 19:36:47 +0500

    forms

---
+++ travel/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from .models import CustomUser, Trip, Impression, Comment
+
+
+# User Login Form
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+# Trip Form
+class TripForm(forms.ModelForm):
+    class Meta:
+        model = Trip
+        fields = ['name', 'location', 'date']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+        }
+
+
+# Impression Form
+class ImpressionForm(forms.ModelForm):
+    class Meta:
+        model = Impression
+        fields = ['title', 'description', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'})
+        }
+
+
+# Comment Form
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
+        }
