f3c907322c98a0f1fc15f2ed47970e494820e87d
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2025-02-26 17:33:13 +0500

    urls update

--- travel/urls.py
+++ travel/urls.py
@@ -3,8 +3,10 @@ from travel import views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
     path('trips/', views.trip_list, name='trip_list'),
+    path('trips/<int:trip_id>/', views.trip_detail, name='trip_detail'),
+    path('profile/', views.profile_view, name='profile'),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
     path('trips/create/', views.trip_create, name='trip_create'),
     path('trips/<int:trip_id>/impressions/create/', views.impression_create, name='impression_create'),
     path('impressions/<int:impression_id>/comments/create/', views.comment_create, name='comment_create'),
