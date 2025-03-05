ce27668c76e04dfdd88f386c5facd6f8631600f6
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2025-03-05 07:34:02 +0500

    settings update

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'travel'
+    'travel',
+    'django_select2',
 ]
 
 MIDDLEWARE = [
